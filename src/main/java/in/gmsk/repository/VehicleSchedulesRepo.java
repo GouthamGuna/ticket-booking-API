655b84d8774300aa14281e61b353e1036da6da46
GouthamGuna <ggowthamsankar@gmail.com>
2023-06-06 07:59:12 +0530

    Update VehicleSchedulesRepo.java

--- src/main/java/in/gmsk/repository/VehicleSchedulesRepo.java
+++ src/main/java/in/gmsk/repository/VehicleSchedulesRepo.java
@@ -1,8 +1,8 @@
 package in.gmsk.repository;
 
-import in.gmsk.model.DailySchedulesForBuses;
+import in.gmsk.model.VehicleDailySchedules;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface DailySchedulesRepo extends JpaRepository<DailySchedulesForBuses, Integer> {
+public interface VehicleSchedulesRepo extends JpaRepository<VehicleDailySchedules, Integer> {
 }
