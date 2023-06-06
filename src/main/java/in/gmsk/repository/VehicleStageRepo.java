3e3d3b694e416fdc1a7dabb421406c706031d10d
GouthamGuna <ggowthamsankar@gmail.com>
2023-06-06 07:59:47 +0530

    Update VehicleStageRepo.java

--- src/main/java/in/gmsk/repository/VehicleStageRepo.java
+++ src/main/java/in/gmsk/repository/VehicleStageRepo.java
@@ -1,2 +1,9 @@
-package in.gmsk.repository;public interface VehicleStageRepo {
+package in.gmsk.repository;
+
+import in.gmsk.model.VehicleStageRegistration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleStageRepo extends JpaRepository<VehicleStageRegistration, Integer> {
 }
