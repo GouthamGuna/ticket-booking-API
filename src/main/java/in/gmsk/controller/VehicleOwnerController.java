c03b168228d8de4a15a721758022c5fe20f9d284
GouthamGuna <ggowthamsankar@gmail.com>
2023-06-11 16:47:01 +0530

    Update VehicleOwnerController.java

--- src/main/java/in/gmsk/controller/VehicleOwnerController.java
+++ src/main/java/in/gmsk/controller/VehicleOwnerController.java
@@ -7,10 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/travels/owners")
@@ -29,5 +28,11 @@ public class VehicleOwnerController {
             @RequestBody @Valid VehicleOwnerRegistration travelOwner){
         logger.info("save the travels owner details : {} ",travelOwner);
         return new ResponseEntity<>(service.saveTravelsOwnerDetails(travelOwner), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fetchAll")
+    public ResponseEntity<List<VehicleOwnerRegistration>> fetchAllOwnersDetails(){
+        logger.info("fetch all travels owners details.");
+        return new ResponseEntity<>(service.fetchAllOwner(), HttpStatus.OK);
     }
 }
