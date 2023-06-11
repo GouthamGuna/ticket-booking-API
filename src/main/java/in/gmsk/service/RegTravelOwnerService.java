cd3a098e85bc575470f85bbbb52587106c5bbb11
GouthamGuna <ggowthamsankar@gmail.com>
2023-06-11 16:47:45 +0530

    Update RegTravelOwnerService.java

--- src/main/java/in/gmsk/service/RegTravelOwnerService.java
+++ src/main/java/in/gmsk/service/RegTravelOwnerService.java
@@ -2,7 +2,11 @@ package in.gmsk.service;
 
 import in.gmsk.model.VehicleOwnerRegistration;
 
+import java.util.List;
+
 public interface RegTravelOwnerService {
 
     VehicleOwnerRegistration saveTravelsOwnerDetails(VehicleOwnerRegistration travelOwner);
+
+    List<VehicleOwnerRegistration> fetchAllOwner();
 }
