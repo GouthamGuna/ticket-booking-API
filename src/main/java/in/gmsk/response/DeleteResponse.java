207548447374683c0a20dc7cdab234cd505537dd
GouthamGuna <ggowthamsankar@gmail.com>
2023-06-12 22:18:04 +0530

    Update DeleteResponse.java

--- src/main/java/in/gmsk/response/DeleteResponse.java
+++ src/main/java/in/gmsk/response/DeleteResponse.java
@@ -1,2 +1,12 @@
-package in.gmsk.response;public class DeleteResponse {
+package in.gmsk.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DeleteResponse {
+
+    private String message;
 }
