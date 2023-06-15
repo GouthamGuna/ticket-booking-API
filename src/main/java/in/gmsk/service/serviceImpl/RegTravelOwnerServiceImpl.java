d4f386611c6bb8cbb2ff20aafd6aba11eb7df22a
GouthamGuna <ggowthamsankar@gmail.com>
2023-06-15 21:59:27 +0530

    Update RegTravelOwnerServiceImpl.java

--- src/main/java/in/gmsk/service/serviceImpl/RegTravelOwnerServiceImpl.java
+++ src/main/java/in/gmsk/service/serviceImpl/RegTravelOwnerServiceImpl.java
@@ -28,6 +28,25 @@ public class RegTravelOwnerServiceImpl implements RegTravelOwnerService {
     }
 
     @Override
+    public VehicleOwnerRegistration updateTravelsOwnerDetails(VehicleOwnerRegistration travelOwner, int id) {
+        VehicleOwnerRegistration exitingOwnerReg = repository.findById(id).orElseThrow(() ->
+                new ResourceNotFound("ServiceImpl", "Update Travel Owner ServiceImpl", id));
+
+        exitingOwnerReg.setOwnerName(travelOwner.getOwnerName());
+        exitingOwnerReg.setOwnerMailId(travelOwner.getOwnerMobileNo());
+        exitingOwnerReg.setMangerAddress(travelOwner.getMangerAddress());
+        exitingOwnerReg.setManagerMailId(travelOwner.getManagerMailId());
+        exitingOwnerReg.setOwnerAddress(travelOwner.getOwnerAddress());
+        exitingOwnerReg.setTravelsName(travelOwner.getTravelsName());
+        exitingOwnerReg.setManagerMobileNo(travelOwner.getManagerMobileNo());
+        exitingOwnerReg.setManagerName(travelOwner.getManagerName());
+
+        repository.save(exitingOwnerReg);
+
+        return exitingOwnerReg;
+    }
+
+    @Override
     public VehicleOwnerRegistration fetchFindById(int id) {
         return repository.findById(id).orElseThrow(() ->
                 new ResourceNotFound("ServiceImpl", "Reg Travel Owner ServiceImpl", id));
