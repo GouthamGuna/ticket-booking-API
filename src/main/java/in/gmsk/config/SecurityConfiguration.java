d42ed4f3958c4fd84caf61955afc1ff378584220
Goutham Guna <ggowthamsankar@gmail.com>
2023-06-22 21:28:31 +0530

    SecurityConfiguration.java

--- src/main/java/in/gmsk/config/SecurityConfiguration.java
+++ src/main/java/in/gmsk/config/SecurityConfiguration.java
@@ -1,2 +1,60 @@
-package in.gmsk.config;public class SecurityConfiguration {
+package in.gmsk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withUsername("gowtham")  // authentication use PasswordEncoder encoder
+                .password(passwordEncoder().encode("Pwd1"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.withUsername("karan")
+                .password(passwordEncoder().encode("Pwd2"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/welcome", "/api/addNewUser").permitAll()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/**").authenticated()
+                .and().formLogin()
+                .and().build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
 }
