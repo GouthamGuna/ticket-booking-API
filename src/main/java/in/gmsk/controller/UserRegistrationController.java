135b82ec2352570b520bc7213bb8a7fd3b603986
Goutham <ggowthamsankar@gmail.com>
2023-06-27 22:09:33 +0530

    Update UserRegistrationController.java

--- src/main/java/in/gmsk/controller/UserRegistrationController.java
+++ src/main/java/in/gmsk/controller/UserRegistrationController.java
@@ -1,2 +1,22 @@
-package in.gmsk.controller;public class UserRegistrationController {
+package in.gmsk.controller;
+
+import in.gmsk.model.UserInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserRegistrationController {
+    private final Logger logger = LoggerFactory.getLogger(UserRegistrationController.class);
+    /*@PostMapping("/addNewUser")
+    public ResponseEntity< ? > addNewUser(@RequestBody UserInfo userInfo) {
+        logger.info("Inside the add new user method.");
+        String output = userService.addUser(userInfo);
+        return new ResponseEntity<>(new SuccessResponseEntity(output), HttpStatus.CREATED);
+    }*/
 }
