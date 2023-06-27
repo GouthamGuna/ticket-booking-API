612d722f5760fab7ec241e0574847aacd81414f5
Goutham <ggowthamsankar@gmail.com>
2023-06-27 22:10:08 +0530

    Uodate UserInfo.java

--- src/main/java/in/gmsk/model/UserInfo.java
+++ src/main/java/in/gmsk/model/UserInfo.java
@@ -1,2 +1,20 @@
-package in.gmsk.model;public class UserInfo {
+package in.gmsk.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String roles;
 }
