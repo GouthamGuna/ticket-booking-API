fd4074a97dc7bf23278eb0a3ca63a9c0fe5c00de
Goutham <ggowthamsankar@gmail.com>
2023-06-27 22:13:11 +0530

    Update UserServiceImpl.java

--- src/main/java/in/gmsk/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/in/gmsk/service/serviceImpl/UserServiceImpl.java
@@ -1,2 +1,24 @@
-package in.gmsk.service.serviceImpl;public class UserServiceImpl {
+package in.gmsk.service.serviceImpl;
+
+import in.gmsk.model.UserInfo;
+import in.gmsk.repository.UserInfoRepo;
+import in.gmsk.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserInfoRepo repository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserInfoRepo repository, PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    @Override
+    public String addUser(UserInfo userInfo) {
+        userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+        repository.save(userInfo);
+        return "Successfully added a user.";
+    }
 }
