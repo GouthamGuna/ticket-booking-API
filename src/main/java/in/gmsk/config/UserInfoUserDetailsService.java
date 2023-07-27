82bf81b0d7fa23008f581ee8a345902aec85a08c
Goutham <ggowthamsankar@gmail.com>
2023-07-27 08:32:11 +0530

    Update UserInfoUserDetailsService.java

--- src/main/java/in/gmsk/config/UserInfoUserDetailsService.java
+++ src/main/java/in/gmsk/config/UserInfoUserDetailsService.java
@@ -1,4 +1,22 @@
 package in.gmsk.config;
 
-public class UserInfoUserDetailsService {
+import in.gmsk.model.UserInfo;
+import in.gmsk.repository.UserInfoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class UserInfoUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserInfoRepo repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserInfo> userInfo = repository.findByName(username);
+        return userInfo.map(UserInfoUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found " + username));
+    }
 }
