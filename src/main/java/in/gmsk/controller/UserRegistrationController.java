5ee7b048f18d060ff01018be344b577050d8fa27
Goutham <ggowthamsankar@gmail.com>
2023-07-27 08:32:45 +0530

    Update UserRegistrationController.java

--- src/main/java/in/gmsk/controller/UserRegistrationController.java
+++ src/main/java/in/gmsk/controller/UserRegistrationController.java
@@ -1,12 +1,13 @@
 package in.gmsk.controller;
 
 import in.gmsk.model.UserInfo;
-import in.gmsk.service.UserService;
+import in.gmsk.response.CreateUserResponse;
+import in.gmsk.service.serviceImpl.UserServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 public class UserRegistrationController {
     private final Logger logger = LoggerFactory.getLogger(UserRegistrationController.class);
-    private UserService userService;
+    @Autowired
+    private UserServiceImpl userService;
 
     @PostMapping("/addNewUser")
    // @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity< ? > addNewUser(@RequestBody UserInfo userInfo) {
         logger.info("Inside the add new user method.");
-        return new ResponseEntity<>(userService.addUser(userInfo), HttpStatus.CREATED);
+        String result = userService.addUser(userInfo);
+        return new ResponseEntity<>(new CreateUserResponse(result), HttpStatus.CREATED);
     }
 }
