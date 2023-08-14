ddff9f29af502a4b2e0ff81e7be394e7cdee172d
Goutham <ggowthamsankar@gmail.com>
2023-08-14 20:11:58 +0530

    Update CreateUserResponse.java

--- src/main/java/in/gmsk/response/CreateUserResponse.java
+++ src/main/java/in/gmsk/response/CreateUserResponse.java
@@ -1,4 +1,13 @@
 package in.gmsk.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class CreateUserResponse {
+
+    private String message;
 }
