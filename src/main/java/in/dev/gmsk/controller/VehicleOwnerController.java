7a7cbfd506cf0b5971e88314ac0a2f07af77e355
Goutham <ggowthamsankar@gmail.com>
2023-11-04 20:45:35 +0530

    Update VehicleOwnerController.java

--- src/main/java/in/dev/gmsk/controller/VehicleOwnerController.java
+++ src/main/java/in/dev/gmsk/controller/VehicleOwnerController.java
@@ -4,8 +4,6 @@ import in.dev.gmsk.model.VehicleOwnerRegistration;
 import in.dev.gmsk.response.DeleteResponse;
 import in.dev.gmsk.service.RegTravelOwnerService;
 import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,7 +15,7 @@ import java.util.List;
 @RequestMapping("/api/v1/travels/owners")
 public class VehicleOwnerController {
 
-    private Logger logger = LoggerFactory.getLogger(VehicleOwnerController.class);
+    // private Logger logger = LoggerFactory.getLogger(VehicleOwnerController.class);
 
     private RegTravelOwnerService service;
 
@@ -29,21 +27,21 @@ public class VehicleOwnerController {
     //@PreAuthorize("hasAuthority('ROLE_USER')")
     public ResponseEntity<VehicleOwnerRegistration> saveTravelsOwners(
             @RequestBody @Valid VehicleOwnerRegistration travelOwner){
-        logger.info("save the travels owner details : {} ",travelOwner);
+       // logger.info("save the travels owner details : {} ",travelOwner);
         return new ResponseEntity<>(service.saveTravelsOwnerDetails(travelOwner), HttpStatus.CREATED);
     }
 
     @GetMapping("/fetchAll")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<List<VehicleOwnerRegistration>> fetchAllOwnersDetails(){
-        logger.info("fetch all travels owners details.");
+      //  logger.info("fetch all travels owners details.");
         return new ResponseEntity<>(service.fetchAllOwner(), HttpStatus.OK);
     }
 
     @GetMapping("/fetchById/{id}")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<VehicleOwnerRegistration> FindById(@PathVariable int id){
-        logger.info("fetch by id travels owners details.");
+     //   logger.info("fetch by id travels owners details.");
         return new ResponseEntity<>(service.fetchFindById(id), HttpStatus.OK);
     }
 
@@ -52,14 +50,14 @@ public class VehicleOwnerController {
     public ResponseEntity<VehicleOwnerRegistration> updateTravelsOwnerDetails(
             @RequestBody @Valid VehicleOwnerRegistration travelOwner,
             @PathVariable int id){
-        logger.info("update by travels owners details.");
+      //  logger.info("update by travels owners details.");
         return new ResponseEntity<>(service.updateTravelsOwnerDetails(travelOwner,id), HttpStatus.OK);
     }
 
     @DeleteMapping("/deleteById/{id}")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity< DeleteResponse > deleteTravelsOwnerDetails(@PathVariable int id){
-        logger.info("delete by id travels owner`s details.");
+       // logger.info("delete by id travels owner`s details.");
         service.deleteTravelsOwnerById(id);
         return new ResponseEntity<>(new DeleteResponse(
                 "The owner's information for Travels has been successfully deleted."), HttpStatus.OK);
