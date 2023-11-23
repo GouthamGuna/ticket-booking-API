8ce0f20cd86a2398f1bb4b9e9e54d5cbc733dbae
GouthamGuna <ggowthamsankar@gmail.com>
2023-11-23 21:18:20 +0530

    Update VehicleOwnerAspect.java

--- src/main/java/in/dev/gmsk/aop/VehicleOwnerAspect.java
+++ src/main/java/in/dev/gmsk/aop/VehicleOwnerAspect.java
@@ -4,6 +4,8 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -11,10 +13,11 @@ import java.util.Date;
 @Aspect
 @Component
 public class VehicleOwnerAspect {
+    private Logger logger = LoggerFactory.getLogger(VehicleOwnerAspect.class);
 
     @Before(value = "execution(* in.dev.gmsk.controller.VehicleOwnerController.*(..))")
     public void beforeCallingMethod(JoinPoint joinpoint){
-        System.out.println("Requesting to "+ joinpoint.getSignature() +" End time : "+ new Date());
+        logger.debug("Requesting to {} End time : "+ new Date(), joinpoint.getSignature());
     }
 
     @After(value = "execution(* in.dev.gmsk.controller.VehicleOwnerController.*(..))")
