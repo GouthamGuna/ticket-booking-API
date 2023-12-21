939ce9ac4c9e1e7bdc00c682062d9e2fddd3ebc3
GouthamGuna <ggowthamsankar@gmail.com>
2023-12-21 23:10:38 +0530

    Update UniqueIdentifierGeneratorServiceTest.java

--- src/test/java/in/dev/gmsk/service/UniqueIdentifierGeneratorServiceTest.java
+++ src/test/java/in/dev/gmsk/service/UniqueIdentifierGeneratorServiceTest.java
@@ -1,4 +1,34 @@
+package in.dev.gmsk.service;
+
+import in.dev.gmsk.service.serviceImpl.UniqueIdentifierGeneratorServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class UniqueIdentifierGeneratorServiceTest {
-  
+    @Autowired
+    private UniqueIdentifierGeneratorServiceImpl service;
+
+    @Test
+    void generateUid() {
+
+        String generateUid = service.generateUid();
+        assertTrue( true );
+
+        System.out.println( "generateUid = " + generateUid );
+    }
+
+    @Test
+    void fromHostAndCurrentTime() {
+
+        UUID uuid = service.fromHostAndCurrentTime();
+        assertTrue( true );
+
+        System.out.println( "uuid = " + uuid );
+    }
 }
